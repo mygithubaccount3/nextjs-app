49ed42ef58edc5cfe2613a40baffd6c89c658b62
mygithubaccount3 <43136479+mygithubaccount3@users.noreply.github.com>
2021-12-10 00:02:42 +0200

    initial commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://nextjs-app:n9ozDo5t59IYBP1V@cluster0.ni1zl.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
